ef320ae576dc0b9cd8c4eb32f5287739ee42199c
alexy <alexyvercruysse@gmail.com>
2016-11-09 15:19:12 +0100

    Add trackerGps + login form + activityRoutes

--- app/src/main/java/com/alexy/webservice/MainActivity.java
+++ app/src/main/java/com/alexy/webservice/MainActivity.java
@@ -1,12 +1,17 @@
 package com.alexy.webservice;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,26 +42,38 @@ public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
     LoginButton loginButton;
-    TextView statusTextViewFacebook;
-    String email,name,gender,birthday;
+    String email,name,gender,birthday,currentCity;
     ProfilePictureView profilePictureView;
+    EditText editTextLogin,editTextPassword;
+    Button connect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        statusTextViewFacebook = (TextView) findViewById(R.id.statusTextViewFacebook);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        connect = (Button) findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RoutesActivity.class);
+                startActivity(intent);
+            }
+        });
         profilePictureView = (ProfilePictureView) findViewById(R.id.image);
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(
-                "public_profile", "email", "user_birthday", "user_friends","user_relationships"));
+                "public_profile", "email", "user_birthday", "user_friends","user_relationships","user_location"));
         callbackManager = CallbackManager.Factory.create();
         updateView();
         AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
             @Override
-            protected void onCurrentAccessTokenChanged(
-                    AccessToken oldAccessToken,
-                    AccessToken currentAccessToken) {
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
             updateView();
             }
         };
@@ -89,8 +106,7 @@ public class MainActivity extends AppCompatActivity {
                                 birthday = object.getString("birthday"); // 01/31/1980 format
                                 gender = object.getString("gender");
                                 name = object.getString("name");
-
-                                statusTextViewFacebook.setText("Vous êtes connecté avec : " + name);
+                                currentCity = object.getJSONObject("location").getString("name");
                                 profilePictureView.setProfileId(object.getString("id"));
                                 profilePictureView.setVisibility(View.VISIBLE);
 
@@ -100,15 +116,46 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
             Bundle parameters = new Bundle();
-            parameters.putString("fields", "id,name,email,gender,birthday,relationship_status");
+            parameters.putString("fields", "id,name,email,gender,birthday,relationship_status,location");
             request.setParameters(parameters);
             request.executeAsync();
 
 
         }
         else {
-            statusTextViewFacebook.setText("Vous n'êtes pas connecté.");
             profilePictureView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    GPSTracker gps = new GPSTracker(this);
+                    if (gps.canGetLocation()){
+                        Log.d("GPS","Latitude : "+gps.getLatitude()+" Longitude : "+gps.getLongitude());
+                    }
+                    else {
+                        Log.d("GPS","Failed");
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to acces location, finish", Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 
