42eb38e6ba2401a619551973877c6a5a4208155a
alexy <alexyvercruysse@gmail.com>
2016-11-09 12:20:19 +0100

    Ajout de fonctionalité facebook

--- app/src/main/java/com/alexy/webservice/MainActivity.java
+++ app/src/main/java/com/alexy/webservice/MainActivity.java
@@ -1,9 +1,13 @@
 package com.alexy.webservice;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +23,13 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 
 
@@ -32,15 +39,17 @@ public class MainActivity extends AppCompatActivity {
     LoginButton loginButton;
     TextView statusTextViewFacebook;
     String email,name,gender,birthday;
+    ProfilePictureView profilePictureView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         statusTextViewFacebook = (TextView) findViewById(R.id.statusTextViewFacebook);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.image);
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(
-                "public_profile", "email", "user_birthday", "user_friends"));
+                "public_profile", "email", "user_birthday", "user_friends","user_relationships"));
         callbackManager = CallbackManager.Factory.create();
         updateView();
         AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
@@ -51,22 +60,6 @@ public class MainActivity extends AppCompatActivity {
             updateView();
             }
         };
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                updateView();
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-
-            }
-        });
     }
 
     @Override
@@ -82,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateView(){
         if (isLoggedIn()){
+
             GraphRequest request = GraphRequest.newMeRequest(
                     AccessToken.getCurrentAccessToken(),
                     new GraphRequest.GraphJSONObjectCallback() {
@@ -97,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                                 name = object.getString("name");
 
                                 statusTextViewFacebook.setText("Vous êtes connecté avec : " + name);
+                                profilePictureView.setProfileId(object.getString("id"));
+                                profilePictureView.setVisibility(View.VISIBLE);
 
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -104,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
             Bundle parameters = new Bundle();
-            parameters.putString("fields", "id,name,email,gender,birthday");
+            parameters.putString("fields", "id,name,email,gender,birthday,relationship_status");
             request.setParameters(parameters);
             request.executeAsync();
 
@@ -112,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             statusTextViewFacebook.setText("Vous n'êtes pas connecté.");
+            profilePictureView.setVisibility(View.INVISIBLE);
         }
     }
+
 }
