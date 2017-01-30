165f8d6a37e07e6e317143ee96a482ef731f006b
AlexyVercruysse <alexyvercruysse@gmail.com>
2017-01-30 13:42:59 +0100

    drive add

--- app/src/main/java/com/alexy/webservice/MainActivity.java
+++ app/src/main/java/com/alexy/webservice/MainActivity.java
@@ -1,13 +1,20 @@
 package com.alexy.webservice;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,134 +36,217 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     CallbackManager callbackManager;
     LoginButton loginButton;
     String email,name,gender,birthday,currentCity;
     ProfilePictureView profilePictureView;
-    EditText editTextLogin,editTextPassword;
     Button connect;
+    GoogleApiClient mGoogleApiClient;
+    private static final int CONNECT = 1;
+    private static final String TAG = "drive-quickstart";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.alexy.webservice",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+
+        }
+
+
+
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 1);
-        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
         connect = (Button) findViewById(R.id.connect);
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,RoutesActivity.class);
-                startActivity(intent);
+                if (isLoggedIn()) {
+                    Intent intent = new Intent(MainActivity.this, RoutesActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    mGoogleApiClient.connect();
+                }
             }
         });
-//        profilePictureView = (ProfilePictureView) findViewById(R.id.image);
-//        loginButton = (LoginButton) findViewById(R.id.login_button);
-//        loginButton.setReadPermissions(Arrays.asList(
-//                "public_profile", "email", "user_birthday", "user_friends","user_relationships","user_location"));
-//        callbackManager = CallbackManager.Factory.create();
-//        updateView();
-//        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
-//            @Override
-//            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
-//            updateView();
-//            }
-//        };
+        profilePictureView = (ProfilePictureView) findViewById(R.id.image);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends","user_relationships","user_location"));
+        callbackManager = CallbackManager.Factory.create();
+        updateView();
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            updateView();
+            }
+        };
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        callbackManager.onActivityResult(requestCode, resultCode, data);
-//    }
-//
-//    public boolean isLoggedIn() {
-//        AccessToken accessToken = AccessToken.getCurrentAccessToken();
-//        return accessToken != null;
-//    }
-//
-//    public void updateView(){
-//        if (isLoggedIn()){
-//
-//            GraphRequest request = GraphRequest.newMeRequest(
-//                    AccessToken.getCurrentAccessToken(),
-//                    new GraphRequest.GraphJSONObjectCallback() {
-//                        @Override
-//                        public void onCompleted(JSONObject object, GraphResponse response) {
-//                            Log.v("LoginActivity", response.toString());
-//
-//                            // Application code
-//                            try {
-//                                email = object.getString("email");
-//                                birthday = object.getString("birthday"); // 01/31/1980 format
-//                                gender = object.getString("gender");
-//                                name = object.getString("name");
-//                                currentCity = object.getJSONObject("location").getString("name");
-////                                profilePictureView.setProfileId(object.getString("id"));
-////                                profilePictureView.setVisibility(View.VISIBLE);
-//
-//                            } catch (JSONException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    });
-//            Bundle parameters = new Bundle();
-//            parameters.putString("fields", "id,name,email,gender,birthday,relationship_status,location");
-//            request.setParameters(parameters);
-//            request.executeAsync();
-//
-//
-//        }
-//        else {
-////            profilePictureView.setVisibility(View.INVISIBLE);
-//        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (isLoggedIn()){
+            startActivity(new Intent(MainActivity.this,RoutesActivity.class));
+        }
+        switch (requestCode) {
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode,
-//                                           String permissions[], int[] grantResults) {
-//        switch (requestCode) {
-//            case 1: {
-//
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    GPSTracker gps = new GPSTracker(this);
-//                    if (gps.canGetLocation()){
-//                        Log.d("GPS","Latitude : "+gps.getLatitude()+" Longitude : "+gps.getLongitude());
-//                    }
-//                    else {
-//                        Log.d("GPS","Failed");
-//                    }
-//
-//                } else {
-//
-//                    // permission denied, boo! Disable the
-//                    // functionality that depends on this permission.
-//                    Toast.makeText(MainActivity.this, "Permission denied to acces location, finish", Toast.LENGTH_SHORT).show();
-//                    this.finish();
-//                }
-//                return;
-//            }
-//
-//            // other 'case' lines to check for other
-//            // permissions this app might request
-//        }
-//    }
+            case CONNECT:
+                if (resultCode == Activity.RESULT_OK) {
+                    mGoogleApiClient.connect();
+                } else {
+                }
+                break;
+        }
 
+
+    }
+
+
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    public void updateView(){
+        if (isLoggedIn()){
+
+            GraphRequest request = GraphRequest.newMeRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            Log.v("LoginActivity", response.toString());
+
+                            // Application code
+                            try {
+                                email = object.getString("email");
+                                birthday = object.getString("birthday"); // 01/31/1980 format
+                                gender = object.getString("gender");
+                                name = object.getString("name");
+                                currentCity = object.getJSONObject("location").getString("name");
+                                profilePictureView.setProfileId(object.getString("id"));
+                                profilePictureView.setVisibility(View.VISIBLE);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,email,gender,birthday,relationship_status,location");
+            request.setParameters(parameters);
+            request.executeAsync();
+            Intent intent = new Intent(MainActivity.this, RoutesActivity.class);
+            startActivity(intent);
+
+
+        }
+        else {
+            profilePictureView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    GPSTracker gps = new GPSTracker(this);
+                    if (gps.canGetLocation()){
+                        Log.d("GPS","Latitude : "+gps.getLatitude()+" Longitude : "+gps.getLongitude());
+                    }
+                    else {
+                        Log.d("GPS","Failed");
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to acces location, finish", Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG,"Connection: "+mGoogleApiClient.isConnected());
+        Intent intent = new Intent(MainActivity.this, RoutesActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG,"Error :"+connectionResult.getErrorCode());
+        if (connectionResult.hasResolution()) {
+
+            try {
+                connectionResult.startResolutionForResult(this, CONNECT);
+            } catch (IntentSender.SendIntentException e) {
+               Log.d(TAG,"StartResolution failed.");
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
+    }
 }
--- app/src/main/java/com/alexy/webservice/RoutesActivity.java
+++ app/src/main/java/com/alexy/webservice/RoutesActivity.java
@@ -1,17 +1,23 @@
 package com.alexy.webservice;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-import com.alexy.webservice.Services.HttpService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -22,6 +28,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,36 +40,68 @@ import static com.alexy.webservice.R.id.spinnerStartStop;
 
 public class RoutesActivity extends AppCompatActivity {
 
-    Spinner spinnerStartStop,spinnerEndStop;
+    Spinner spinnerStartStop, spinnerEndStop;
     private String value;
+    Button btn2,btn3;
+    ArrayAdapter<String> adapterStart;
+    ArrayAdapter<String> adapterEnd;
+    String startResult;
+    String endResult;
+    String end = "1";
+    String start = "1";
+    Boolean isEnd = false;
+    TextView resultTV;
+    String temps = "";
+    private AsyncHttpClient client;
+    private AsyncHttpClient client2;
+    private AsyncHttpClient client3;
+    private AsyncHttpClient client4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
         spinnerStartStop = (Spinner) findViewById(R.id.spinnerStartStop);
         spinnerEndStop = (Spinner) findViewById(R.id.spinnerEndStop);
-
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://192.168.240.36/index.php/Stop/All", new AsyncHttpResponseHandler() {
+        resultTV =(TextView) findViewById(R.id.resultTV);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(RoutesActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        temps = "&hour="+i+":"+i1;
+                        btn3.setText("Heure d'arrivée : "+i+":"+i1);
+                    }
+                }, new Date().getHours(),new Date().getMinutes(),true);
+                timePickerDialog.show();
+            }
+        });
+        client = new AsyncHttpClient();
+        client.get("http://192.168.240.36/index.php/stop/list", new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 ObjectMapper mapper = new ObjectMapper();
                 try {
-                    Map<String, Object> result  = mapper.readValue(responseBody, Map.class);
+                    Map<String, Object> result = mapper.readValue(responseBody, Map.class);
                     List<String> listStops = new ArrayList<>();
 
-                    for(String str : result.keySet()){
-                        Map<String, Object> stop = (Map<String,Object>)result.get(str);
+                    for (String str : result.keySet()) {
+                        Map<String, Object> stop = (Map<String, Object>) result.get(str);
                         String nameStop = (String) stop.get("name");
+                        Log.d("Stops :",""+nameStop);
                         listStops.add(nameStop);
-                    }
 
-                    ArrayAdapter<String> adapterStart = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, listStops);
-                    ArrayAdapter<String> adapterEnd = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, listStops);
+                    }
+                    Log.d("Stops :",""+listStops);
+                    adapterStart = new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, listStops);
+                    adapterEnd = new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, listStops);
                     adapterStart.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     adapterEnd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerStartStop.setAdapter(adapterStart);
                     spinnerEndStop.setAdapter(adapterEnd);
+
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -72,12 +111,132 @@ public class RoutesActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 error.printStackTrace();
-                Log.e("HttpError","GET REQUEST ERROR" + error.getMessage());
+                Log.e("HttpError", "GET REQUEST ERROR" + error.getMessage());
+            }
+        });
+
+        spinnerStartStop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                start = spinnerStartStop.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinnerEndStop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                end = spinnerEndStop.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (adapterEnd.isEmpty() || adapterStart.isEmpty()) {
+
+                } else {
+                    executeAll();
+                }
+
+
             }
         });
 
 
-
-
     }
+
+
+    public void executeAll(){
+        client2 = new AsyncHttpClient();
+        Log.d("Details","http://192.168.240.36/index.php/stop?name=" + start);
+        client2.get("http://192.168.240.36/index.php/stop?name=" + start, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    startResult = mapper.readValue(responseBody, String.class);
+
+
+                    client3 = new AsyncHttpClient();
+                    Log.d("Details","http://192.168.240.36/index.php/stop?name=" + end);
+                    client3.get("http://192.168.240.36/index.php/stop?name=" + end, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            ObjectMapper mapper = new ObjectMapper();
+                            try {
+                                endResult = mapper.readValue(responseBody, String.class);
+
+                                client4 = new AsyncHttpClient();
+                                Log.d("Details","http://192.168.240.36/index.php/hour/travel/stops/id?idStartStop=" + startResult + "&idEndStop=" + endResult + temps);
+                                client4.get("http://192.168.240.36/index.php/hour/travel/stops/id?idStartStop=" + startResult + "&idEndStop=" + endResult + temps, new AsyncHttpResponseHandler() {
+                                    @Override
+                                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                        ObjectMapper mapper = new ObjectMapper();
+                                        try {
+                                            Map<String, Object> result = mapper.readValue(responseBody, Map.class);
+                                            List<String> listStops = new ArrayList<>();
+
+                                            for (String str : result.keySet()) {
+                                                Map<String, Object> stop = (Map<String, Object>) result.get(str);
+                                                String nameStop = (String) stop.get("hour");
+                                                listStops.add(nameStop);
+                                                Log.d("test",nameStop);
+
+                                            }
+                                            Log.d("tag", listStops + "");
+                                            if (listStops.get(0).isEmpty() || listStops.get(1).isEmpty()){
+                                                resultTV.setText("Pas d'horraire");
+                                            }
+                                            resultTV.setText("Départ : "+ listStops.get(0) + "\nArrivé : "+ listStops.get(1));
+                                            resultTV.setVisibility(View.VISIBLE);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+
+                                    @Override
+                                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                                    }
+                                });
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                        }
+                    });
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+
+
 }
